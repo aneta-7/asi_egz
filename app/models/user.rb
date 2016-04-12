76cf0d41c1619776537e698a6695f3a3b225dc6a
romety2 <romety2@wp.pl>
2016-04-12 22:36:47 +0000

    usuwanie CASCADE

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  has_many :links
+  has_many :links, :dependent => :delete_all
+  has_many :comments, :dependent => :delete_all
 end
